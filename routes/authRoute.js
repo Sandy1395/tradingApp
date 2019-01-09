05cb836332e44c14032ca8c7cf1c94ffe6eafb91
sandip <sandippurohit1395@gmail.com>
2019-01-10 02:48:20 +0530

    trading app angular+node

---
+++ routes/authRoute.js
@@ -0,0 +1,31 @@
+var express = require('express');
+const jwt = require('jsonwebtoken');
+const {  check, validationResult } = require('express-validator/check');
+var router = express.Router();
+const db = require('../Database/connection');
+const { login } = require('../Database/queries');
+
+router.post('/login', [
+    check('email').exists().withMessage('email Required'),
+    check('password').exists().withMessage('password required')
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const err = errors.array().map(err => ({ field: err.param, message: err.msg }));
+        return res.status(422).json(err);
+    }
+    const { email, password } = req.body;
+    db.query(login, [email, password], (err, results, fields) => {
+        if(err) {
+            res.status(501).json({ message: 'Internal server error', err });
+        } else if(results.length === 1) {
+            const SECRET = process.env.SECRET;
+            const token = jwt.sign({ ...results[0], loggedIn: true }, SECRET, { expiresIn: '1d' });
+            res.send({ token, loggedIn: true, ...results[0] });
+        } else {
+            res.status(422).json({ message: 'Invalid credentials.' });
+        }
+    })
+});
+
+module.exports = router;
